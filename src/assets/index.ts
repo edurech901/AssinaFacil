5ebb9627772f05d7534f28ea478538c9af0411fa
Kalvin Reiter <koolvinffr@gmail.com>
2026-06-29 22:44:01 -0300

    Implementando o menu lateral

---
+++ src/assets/index.ts
@@ -0,0 +1,13 @@
+import iconeDashboard from './icone-dashboard.svg';
+import iconeAssinaturas from './icone-assinaturas.svg';
+import iconeNovaAssinatura from './icone-nova-assinatura.svg';
+import iconeConfig from './icone-config.svg';
+import iconeSair from './icone-sair.svg';
+
+export {
+  iconeDashboard,
+  iconeAssinaturas,
+  iconeNovaAssinatura,
+  iconeConfig,
+  iconeSair
+};
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
+import Layout from '../components/Layout/Layout';
 import Dashboard from '../pages/Dashboard/Dashboard';
 import Subscriptions from '../pages/Subscriptions/Subscriptions';
 import NewSubscription from '../pages/NewSubscription/NewSubscription';
@@ -8,11 +9,13 @@ import SubscriptionDetails from '../pages/SubscriptionDetails/SubscriptionDetail
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/subscriptions" element={<Subscriptions />} />
-      <Route path="/subscriptions/new" element={<NewSubscription />} />
-      <Route path="/subscriptions/:id" element={<SubscriptionDetails />} />
-      <Route path="/subscriptions/:id/edit" element={<EditSubscription />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/subscriptions" element={<Subscriptions />} />
+        <Route path="/subscriptions/new" element={<NewSubscription />} />
+        <Route path="/subscriptions/:id" element={<SubscriptionDetails />} />
+        <Route path="/subscriptions/:id/edit" element={<EditSubscription />} />
+      </Route>
     </Routes>
   );
 }
